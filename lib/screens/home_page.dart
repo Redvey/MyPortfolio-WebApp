341ccbb0868be6f47d064938ec70416e49350b90
redvey <roopam12cse@gmail.com>
2023-12-03 20:37:55 +0530

    Added a clock and text animations

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,16 +1,20 @@
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:custom_button_builder/custom_button_builder.dart';
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:o3d/o3d.dart';
 import 'package:portfolio/consts/data.dart';
 import 'package:portfolio/providers/current_state.dart';
+import 'package:portfolio/screens/mini_projects/clocky/clockview.dart';
 import 'package:portfolio/screens/phone_screen_wrapper.dart';
 import 'package:portfolio/widgets/frosted_containers.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 
@@ -29,6 +33,20 @@ class _HomePageState extends State<HomePage> {
     Size size= MediaQuery.of(context).size;
     CurrentState currentState =
        Provider.of<CurrentState>(context,listen: false);
+
+    //animatedtext
+    // const colorizeColors = [
+    //   Colors.orange,
+    //   Colors.white,
+    //   Colors.black,
+    //   Colors.green,
+    // ];
+    // const colorizeTextStyle = TextStyle(
+    //   fontSize: 22,
+    //   fontFamily: 'Agne',
+    //   fontWeight: FontWeight.bold,
+    // );
+
     return  Scaffold(
       body: Stack(
         children: [
@@ -66,7 +84,7 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         SizedBox(
                           height: 500,
-                          width: 300, // Set the width of the O3D widget
+                          width: 225, // Set the width of the O3D widget
                           child: O3D(
                             src: 'assets/animations/robot_32x.glb',
                             controller: o3dController,
@@ -103,21 +121,45 @@ class _HomePageState extends State<HomePage> {
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        FrostedContainer(
+                        Container(
                           height: 150,
-                          width: 300,
-                          childG: Center(child: Text("data")),
+                          width: 225,
+                          child: Center(child: ClockView(size: MediaQuery.of(context).size.height / 4,)),
                         ),
                         SizedBox(height: 10),
                         FrostedContainer(
                           height: 150,
-                          width: 300,
-                          childG: Center(child: Text("data")),
+                          width: 225,
+                          childG: Center(child:
+                          DefaultTextStyle(
+                            style:  GoogleFonts.dancingScript(
+                              fontSize: 14.0,
+                              color: Colors.white
+                            ),
+                            child: AnimatedTextKit(
+                              animatedTexts: [
+                                TypewriterAnimatedText('Hire me!'),
+                                TypewriterAnimatedText("This is Roopam's portfolio website"),
+                              ],
+                              onTap: () async {
+                                String email = Uri.encodeComponent("roopam12cse@gmail.com");
+                                String subject = Uri.encodeComponent("Job Offer: [Position Title] - [Your Company Name]");
+                                String body = Uri.encodeComponent("Hi! I'm <type your name>");
+                                Uri mail = Uri.parse("mailto:$email?subject=$subject&body=$body");
+                                if (await launchUrl(mail)) {
+                                  // Email app opened
+                                } else {
+                                  // Email app is not opened
+                                }
+                              },
+                            ),
+                          ),
+                          ),
                         ),
                         SizedBox(height: 10),
                         FrostedContainer(
                           height: 150,
-                          width: 300,
+                          width: 225,
                           childG: Center(
                             child: Wrap(
                               children: [
